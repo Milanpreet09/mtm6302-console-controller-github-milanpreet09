af20e6a18bd17e1b0973277f44702f9e1bab89c2
Milanpreet09 <gillmilan25@gmail.com>
2024-06-14 18:38:36 -0400

    uploading index.html, style.css, script.js

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-// function to set title
+//functionn to set title
 function setTitle(newTitle)
 {
     const titleElement = document.getElementById('title');
     titleElement.textContent = newTitle;
 }
-
-// Function for description.
- function setDescription
-(newDescription)
+//Function for description.
+function setDescription(newDescription)
 {
     const descriptionElement = document.getElementById('description');
     descriptionElement.textContent = newDescription;
+
 }
 
-// Function to set background color.
-function setBackgroundColor(color)
+//Function to set background color.
+function setBackgroundcolor(color)
 {
- document.body.style.backgroundColor = color;
+    document.body.style.backgroundcolor = color;
 }
-
-// Function to set font color.
+//Function to set the font color.
 function setFontColor(color)
-
 {
     document.body.style.color = color;
-    const allElements = document.querySelectorAll('*')
-    allElements.forEach( element => {element.style.color = color});
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach(element => {element.style.color = color});
+
 }
-// Funtion to set Theme
+//Function to set the Theme
 function setTheme(theme)
 {
-    if ( theme === 'dark')
-        { document.body.classList.add ('dark-theme');
-            document.body.classList.remove(light-color);
+    if(theme === 'dark')
+        { 
+            document.body.classList.add('dark-theme');
+            document.body.classList.remove('light-theme');
         }
-    
-    
-     else if (theme === 'light'){
-          document.body.classList.add ('light-theme');
-    document.body.classList.remove(dark-color);
-}}
+          else if(theme === 'light')
+    { 
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+    }
+}
